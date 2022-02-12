abb3ffdab6f00c1b1d185f20bd2db31e3cf0a0fc
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2022-02-11 23:55:28 -0500

    implement basic quiz game
    - get values to add and expected value from from csv file
    - show values to add to the user in command prompt, and accept user input
    - compare user input to expected value
    - keep running sum of the number of correct inputs
    - return the percentage of questions that they got correct

--- quiz-game/quiz-game.go
+++ quiz-game/quiz-game.go
@@ -1,19 +1,45 @@
 package main
 
-import ("fmt"; "os"; "strconv")
+import ("encoding/csv";"fmt"; "os"; "strconv"; "strings")
 
 func add(x int, y int) int {
 	return x + y
 }
 
-func strToI(str string) int{
+func strToI(str string) int {
   val, err := strconv.Atoi(str)
-  if err !=nil { fmt.Println(err) }
+  if err != nil { fmt.Println(err) }
 
   return val
 }
 
+func toPercentage(num int, den int) float64 {
+  return (float64(num) / float64(den)) * 100
+}
+
 func main() {
-  args := os.Args[1:]
-  fmt.Printf("%v + %v = %v\n", args[0], args[1], add(strToI(args[0]), strToI(args[1])))
+  // args := os.Args[1:]
+  // fmt.Printf("%v + %v = %v\n", args[0], args[1], add(strToI(args[0]), strToI(args[1])))
+
+  f, _ := os.Open("quiz.csv")
+  defer f.Close()
+
+  csvReader := csv.NewReader(f)
+  data, _   := csvReader.ReadAll()
+  score     := 0
+
+  for _, v := range data {
+    values := strings.Split(v[0], "+")
+    // fmt.Printf("%v %v %v\n", values[0], values[1], v[1])
+
+    fmt.Printf("%v + %v = ", values[0], values[1])
+
+    var input string
+
+    fmt.Scanln(&input)
+    if input == v[1] { score++ }
+  }
+
+  fmt.Printf("Your Score: %.2f", toPercentage(score, len(data)))
+
 }
