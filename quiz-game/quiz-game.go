3d8cf551c8ff8ee3d8673cb65575601bec8e9454
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2022-02-13 23:33:24 -0500

    implement quiz time constraint using time package and go routine

--- quiz-game/quiz-game.go
+++ quiz-game/quiz-game.go
@@ -1,38 +1,57 @@
 package main
 
 import ("encoding/csv"
+        "flag"
         "fmt"
         "os"
         // "strconv"
         "strings"
+        "time"
 )
 
 func main() {
   // args := os.Args[1:]
   // fmt.Printf("%v + %v = %v\n", args[0], args[1], add(strToI(args[0]), strToI(args[1])))
 
-  f, _ := os.Open("quiz.csv")
+  csvFilename := flag.String("csv", "quiz.csv", "a csv file in the format of 'question,answer'")
+  timeLimit   := flag.Int("max_time", 30, "number of seconds allowed for quiz")
+  flag.Parse()
+
+  f, _ := os.Open(*csvFilename)
   defer f.Close()
 
   csvReader := csv.NewReader(f)
   data, _   := csvReader.ReadAll()
   score     := 0
 
+  var input string
+  fmt.Println("Press enter to begin.")
+  fmt.Scanln(&input)
+
+  // start timer once they press enter
+  timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+quizLoop:
   for _, v := range data {
     values := strings.Split(v[0], "+")
-    // fmt.Printf("%v %v %v\n", values[0], values[1], v[1])
-
     fmt.Printf("%v + %v = ", values[0], values[1])
 
-    var input string
+    inputChan := make(chan string)
 
-    fmt.Scanln(&input)
+    go func() {
+      fmt.Scanln(&input)
+  		inputChan <- strings.TrimSpace(input)
+    }()
 
-    if input == v[1] { score++ }
+    select {
+      case <-inputChan:
+        if input == v[1] { score++ }
+      case <-timer.C:
+        break quizLoop // need to break this way to exit both select and loop
+    }
   }
 
-  fmt.Printf("Your Score: %.2f", toPercentage(score, len(data)))
-
+  fmt.Printf("\nYour Score: %.2f", toPercentage(score, len(data)))
 }
 
 func toPercentage(num int, den int) float64 {
